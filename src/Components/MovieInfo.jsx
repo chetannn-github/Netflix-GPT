c96225514af47e3cb009e9c82f142768ceb7cd9d
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-06-04 16:01:00 +0530

    completed  movie info page

--- src/Components/MovieInfo.jsx
+++ src/Components/MovieInfo.jsx
@@ -1,25 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { TMDB_API_OPTIONS } from '../../public/utils/Constants';
 import VideoBg from './VideoBg';
 import "../Stylesheets/MovieInfo.css"
+import "../Stylesheets/MainMovie.css"
 
 const MovieInfo = () => {
+    let [movieInfo ,setMovieInfo] = useState(null);
     let{ id }= useParams();
-    // useEffect(()=>{fetchMovieInfo(id),[]});
+    useEffect(()=>{fetchMovieInfo(id),[]});
 
-    // let fetchMovieInfo = async (id) =>{
-    //     const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
-    //     let data = await fetch(url, TMDB_API_OPTIONS);
-    //     let movieInfo =await data.json();
-    //    console.log(movieInfo);
+    let fetchMovieInfo = async (id) =>{
+        const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+        let data = await fetch(url, TMDB_API_OPTIONS);
+        let movieInfo =await data.json();
+        setMovieInfo(movieInfo);
+       
+}
 
+    if (!movieInfo) return
+    let {title,overview,}= movieInfo;
 
-
-
-    // }
-  return ( <div id="movieinfo">
+return ( <div id="movieinfo-container">
         <VideoBg movieId={id}/>
+        <div id="movie-info">
+            <div id="title">{title}</div>
+            <div id= "overview">{overview}</div>
+            <div id="buttons">
+                <div id="play">▶   Play</div>
+                <div id="more">More Info</div>
+            </div>
+        </div>
         </div>
   )
 }
